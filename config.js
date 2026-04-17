68620f5a5dd56838339d8cc548fde13906ef9725
dev jal <newagesocietyaltsoc@gmail.com>
2026-04-17 19:48:20 +0000

    c3

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 const admin = require('firebase-admin')
 const path = require('path')
 
- const serviceAccountPath = path.resolve(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS || './actsfellowshipintl-service-account.json')
- const serviceAccount = require(serviceAccountPath)
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
-// if (serviceAccount.private_key) {
-//   serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n')
-// }
+// Local development - uncomment these lines for local mode:
+// const serviceAccountPath = path.resolve(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS || './actsfellowshipintl-service-account.json')
+// const serviceAccount = require(serviceAccountPath)
+
+// Production - uses environment variable:
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
+if (serviceAccount.private_key) {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n')
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
